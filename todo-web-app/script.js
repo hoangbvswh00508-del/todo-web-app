6948e5e8abec3ecfb651873fa2bd1c418c7cdebf
Hoang <hoangbiku@gmail.com>
2025-11-07 17:57:40 +0700

    Add script.js file and basic functions

--- todo-web-app/script.js
+++ todo-web-app/script.js
@@ -11,3 +11,14 @@ function sum(a, b) {
 hello();
 let sum2num = sum(2, 1);
 console.log("Sum 2 and 1 are: " + sum2num);
+
+function inMauSo() {
+    for ( let i = 1; i <= 5; i++) {
+        if (i % 2 === 0) {
+            console.log(`[Done] The work number ${i}: Check the code.`);
+        } else {
+            console.log(`[Done] The work number ${i}: write the document`);
+        }
+    }
+}
+inMauSo();
--- todo-web-app/test.js
+++ todo-web-app/test.js
@@ -2,3 +2,15 @@ let ten = "Hoang";
 const TUOI = 23;
 console.log(typeof ten);
 console.log(typeof TUOI);
+
+//Vòng lặp
+for (let i = 1; i <= 5; i++) {
+    console.log("Mẫu số: " + i);
+}
+//Điều kiện
+let timeWork = 10;
+if (timeWork < 12) {
+    console.log("Morning, let's coding!");
+} else {
+    console.log("It's afternoon, fasterrrr!");
+}
