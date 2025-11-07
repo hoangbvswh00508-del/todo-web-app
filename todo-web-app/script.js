6ce110d267bda6e7c828f9dcb66e3ce640c584e4
Hoang <hoangbiku@gmail.com>
2025-11-07 12:31:49 +0700

    Add script.js file and basic functions

---
+++ todo-web-app/script.js
@@ -0,0 +1,13 @@
+
+function hello() {
+    console.log("to-do List app installed succesfully")
+}
+
+function sum(a, b) {
+    let result = a + b;
+    return result;
+}
+
+hello();
+let sum2num = sum(2, 1);
+console.log("Sum 2 and 1 are: " + sum2num);
